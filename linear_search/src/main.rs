868a2322f88d04f968b8b2f55bf1930fcbf92f91
Sandeep Chakraborty <myselfsandyy@Sandeeps-MacBook-Pro.local>
2021-10-05 19:33:56 +0530

    Modified 'Linear Search' code on 5th Oct., 2021

--- linear_search/src/main.rs
+++ linear_search/src/main.rs
@@ -9,12 +9,19 @@ pub fn linear_search<T: PartialEq>(item: &T, arr: &[T]) -> i32 {
     idx_pos
 }
 fn main() {
-    let index = linear_search(&"Rust", &vec!["Python", "Php", "Java", "C", "C++", "Rust"]);
-    println!("Position: {}", index);
+    let index = linear_search(&25, &vec![77, 25, -17, 1, 48, 7]);
+    if index >= 0 {
+        println!("Element found at position: {}", index);
+    }
+    else {
+        println!("Element not found");
+    }
 
-    let index = linear_search(&25, &vec![25, 62, 29, 43, 77]);
-    println!("Position: {}", index);
-
-    let index = linear_search(&855, &vec![25, 62, 29, 43, 77]);
-    println!("Position: {}", index);
+    let index = linear_search(&855, &vec![89, 90, 12, 9, 31, 6]);
+    if index >= 0 {
+        println!("Element found at position: {}", index);
+    }
+    else {
+        println!("Element not found");
+    }
 }
