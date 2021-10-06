4ebd9a99aaf4dbee656d4fdaf642710ae0e1b223
Sandeep Chakraborty <myselfsandyy@Sandeeps-MacBook-Pro.local>
2021-10-06 21:17:59 +0530

    Modified 'Count of Leap Years' code

--- leap_year/src/main.rs
+++ leap_year/src/main.rs
@@ -1,42 +1,21 @@
-use std::io;
-fn check_leap(year_input: i32) -> bool {
-    if year_input % 4 == 0 {
-        if year_input % 100 == 0 {
-            if year_input % 400 == 0 {
-                true
+fn main() {
+    let years_arr: [i32; 11] = [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010];
+    let mut count = 0;
+    for i in 0..11 {
+        if years_arr[i] % 4 == 0 {
+            if years_arr[i] % 100 == 0 {
+                if years_arr[i] % 400 == 0 {
+                    count = count + 1;
+                }
+                else {
+                    }
             }
             else {
-                false
+                count = count + 1;
             }
         }
         else {
-            true
         }
     }
-    else {
-        false
-    }
-} 
-fn main() {
-    let mut count = 0;
-    println!("Enter starting year: ");
-    let mut year = String::new();
-    io::stdin().read_line(&mut year).expect("error occured while reading");
-    let year_int1: i32 = year.trim().parse::<i32>().unwrap();
-
-    println!("Enter ending year: ");
-    let mut year = String::new();
-    io::stdin().read_line(&mut year).expect("error occured while reading");
-    let year_int2: i32 = year.trim().parse::<i32>().unwrap();
-
-    //let i = ;
-    for i in year_int1..year_int2 {
-        let x: bool = check_leap(i);
-        if x {
-            count = count + 1;
-        }
-        else {
-        }
-    }
-    println!("No. of leap years: {}", count);
+    println!("Count of Leap Years is: {}", count);
 }
