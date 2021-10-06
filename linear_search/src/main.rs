3370b218286cfa9335b2705c7b4c836ed143a848
Sandeep Chakraborty <myselfsandyy@Sandeeps-MacBook-Pro.local>
2021-10-06 21:15:57 +0530

    Modified 'Linear Search' code

--- linear_search/src/main.rs
+++ linear_search/src/main.rs
@@ -1,7 +1,7 @@
-pub fn linear_search<T: PartialEq>(item: &T, arr: &[T]) -> i32 {
+fn linear_search(item: i32, arr: &[i32]) -> i32 {
     let mut idx_pos = -1; 
     for (idx, data) in arr.iter().enumerate() {
-        if item == data {
+        if item == *data {
             idx_pos = idx as i32;
             return idx_pos;
         }
@@ -9,15 +9,9 @@ pub fn linear_search<T: PartialEq>(item: &T, arr: &[T]) -> i32 {
     idx_pos
 }
 fn main() {
-    let index = linear_search(&25, &vec![77, 25, -17, 1, 48, 7]);
-    if index >= 0 {
-        println!("Element found at position: {}", index);
-    }
-    else {
-        println!("Element not found");
-    }
-
-    let index = linear_search(&855, &vec![89, 90, 12, 9, 31, 6]);
+    let item = -17;
+    let arr: [i32; 6] = [77, 25, -17, 1, 48, 7];
+    let index = linear_search(item, &arr);
     if index >= 0 {
         println!("Element found at position: {}", index);
     }
