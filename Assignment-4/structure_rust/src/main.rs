f156f3d6fc0118bc978577c6819b38e90892bd4b
Sandeep Chakraborty <myselfsandyy@Sandeeps-MacBook-Pro.local>
2021-10-13 17:37:44 +0530

    Commenting in 'main.rs' file

--- Assignment-4/structure_rust/src/main.rs
+++ Assignment-4/structure_rust/src/main.rs
@@ -1,4 +1,6 @@
 #[derive(Debug)]
+
+// Student Structure
 struct Student {
     name: String,
     roll_no: i32,
@@ -7,6 +9,7 @@ struct Student {
     school: String,
 }
 
+// Score Structure
 struct Score {
     hindi: f32,
     english: f32,
@@ -14,79 +17,143 @@ struct Score {
     science: f32,
 }
 
-fn new(name: String, roll_no: i32, score_of_each_subject: i32, department: String, school: String) -> Student {
+// This method initializes Student objects
+//
+// #Arguments
+//
+// Student Structure
+//
+// #Return
+//
+// Return the Student type objects
+fn new(
+    name: String,
+    roll_no: i32,
+    score_of_each_subject: i32,
+    department: String,
+    school: String,
+) -> Student {
     Student {
-        name: name,
-        roll_no: roll_no,
-        score_of_each_subject: score_of_each_subject,
-        department: department,
-        school: school,
+        name,
+        roll_no,
+        score_of_each_subject,
+        department,
+        school,
     }
 }
 
-fn get_average(average: &Score) -> f32{
+// This method is to get average of all scores
+//
+// #Arguments
+//
+// Score Structure
+//
+// #Return
+//
+// Return the average of marks
+fn get_average(average: &Score) -> f32 {
     let avg: f32 = (average.hindi + average.english + average.maths + average.science) / 4.0;
     avg
 }
 
-fn pass_student(marks: &Score) -> [f32; 4]{
+// This method add numbers to student's score if score < 35
+//
+// #Arguments
+//
+// Score Structure
+//
+// #Return
+//
+// Return the array of new marks stored in it
+fn pass_student(marks: &Score) -> [f32; 4] {
     let mut arr: [f32; 4] = [marks.hindi, marks.english, marks.maths, marks.science];
 
     let diff1: f32;
     let diff2: f32;
-    let diff3: f32; 
+    let diff3: f32;
     let diff4: f32;
     if arr[0] < 35.0 {
-        diff1 = 35.0 - arr[0];
-        arr[0] = arr[0] + diff1;
+        diff1 = 35.0 - arr[0]; // Stores subtracted score from 35
+        arr[0] += diff1;
     }
     if arr[1] < 35.0 {
         diff2 = 35.0 - arr[1];
-        arr[1] = arr[1] + diff2;
+        arr[1] += diff2;
     }
     if arr[2] < 35.0 {
         diff3 = 35.0 - arr[2];
-        arr[2] = arr[2] + diff3;
+        arr[2] += diff3;
     }
     if arr[3] < 35.0 {
         diff4 = 35.0 - arr[3];
-        arr[3] = arr[3] + diff4;
+        arr[3] += diff4;
     }
-    return arr;
+    arr
 }
 
+// Implementing Score Structure
 impl Score {
+    // This method is to print difference of each subject's score b/w students
+    //
+    // #Arguments
+    //
+    // Score Structure
+    //
+    // #Return
+    //
+    // No Return
     fn compare_student(&self, other: &Score) {
         if self.hindi >= other.hindi {
-            println!("Student_1 has higher marks in Hindi by: {}", self.hindi - other.hindi);
-        }
-        else {
-            println!("Student_2 has higher marks in Hindi by: {}", other.hindi - self.hindi);
+            println!(
+                "Student_1 has higher marks in Hindi by: {}",
+                self.hindi - other.hindi
+            );
+        } else {
+            println!(
+                "Student_2 has higher marks in Hindi by: {}",
+                other.hindi - self.hindi
+            );
         }
 
         if self.english >= other.english {
-            println!("Student_1 has higher marks in English by: {}", self.english - other.english);
-        }
-        else {
-            println!("Student_2 has higher marks in English by: {}", other.english - self.english);
+            println!(
+                "Student_1 has higher marks in English by: {}",
+                self.english - other.english
+            );
+        } else {
+            println!(
+                "Student_2 has higher marks in English by: {}",
+                other.english - self.english
+            );
         }
 
         if self.maths >= other.maths {
-            println!("Student_1 has higher marks in Maths by: {}", self.maths - other.maths);
-        }
-        else {
-            println!("Student_2 has higher marks in Maths by: {}", other.maths - self.maths);
+            println!(
+                "Student_1 has higher marks in Maths by: {}",
+                self.maths - other.maths
+            );
+        } else {
+            println!(
+                "Student_2 has higher marks in Maths by: {}",
+                other.maths - self.maths
+            );
         }
 
         if self.science >= other.science {
-            println!("Student_1 has higher marks in Science by: {}", self.science - other.science);
-        }
-        else {
-            println!("Student_2 has higher marks in Science by: {}", other.science - self.science);
+            println!(
+                "Student_1 has higher marks in Science by: {}",
+                self.science - other.science
+            );
+        } else {
+            println!(
+                "Student_2 has higher marks in Science by: {}",
+                other.science - self.science
+            );
         }
     }
 }
 
+// Main function
 fn main() {
     let mut student1_score = Score {
         hindi: 32.0,
@@ -102,8 +169,18 @@ fn main() {
         science: 35.0,
     };
 
-    let arr1 = [student1_score.hindi, student1_score.english, student1_score.maths, student1_score.science];
-    let arr2 = [student2_score.hindi, student2_score.english, student2_score.maths, student2_score.science];
+    let arr1 = [
+        student1_score.hindi,
+        student1_score.english,
+        student1_score.maths,
+        student1_score.science,
+    ];
+    let arr2 = [
+        student2_score.hindi,
+        student2_score.english,
+        student2_score.maths,
+        student2_score.science,
+    ];
     println!("Present scores of Student_1: {:?}", arr1);
     println!("Present scores of Student_2: {:?}", arr2);
 
@@ -130,7 +207,7 @@ fn main() {
         maths: array2[2],
         science: array2[3],
     };
-    
+
     println!();
     println!("New Average of Student_1: {}", get_average(&student1_score));
     println!("New Average of Student_2: {}", get_average(&student2_score));
@@ -139,6 +216,24 @@ fn main() {
     student1_score.compare_student(&student2_score);
 
     println!();
-    println!("Student_1: {:?}",new(String::from("Sandeep"), 44, 67, String::from("CSE"), String::from("AEC")));
-    println!("Student_2: {:?}",new(String::from("Suman"), 38, 32, String::from("CSE"), String::from("KIIT")));
+    println!(
+        "Student_1: {:?}",
+        new(
+            String::from("Sandeep"),
+            44,
+            67,
+            String::from("CSE"),
+            String::from("AEC")
+        )
+    );
+    println!(
+        "Student_2: {:?}",
+        new(
+            String::from("Suman"),
+            38,
+            32,
+            String::from("CSE"),
+            String::from("KIIT")
+        )
+    );
 }
