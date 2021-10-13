008e8cb8fb44ad8fbce4800466a415f6025b45c3
Sandeep Chakraborty <myselfsandyy@Sandeeps-MacBook-Pro.local>
2021-10-13 17:38:05 +0530

    Commenting in 'main.rs' file

--- Assignment-4/complex_numbers/src/main.rs
+++ Assignment-4/complex_numbers/src/main.rs
@@ -1,25 +1,56 @@
+// Numbers Structure
 struct Numbers {
     first_number: u32,
     second_number: u32,
 }
 
+// Implmenting Numbers Structure
 impl Numbers {
+    // This method adds numbers using structures to function
+    //
+    // #Arguments
+    //
+    // Instance of the Structure Number
+    //
+    // #Return
+    //
+    // Returns addition of the two numbers
     fn add(&self) -> u32 {
         self.first_number + self.second_number
     }
+
+    // This method substract numbers using structures to function
+    //
+    // #Arguments
+    //
+    // Instance of the Structure Number
+    //
+    // #Return
+    //
+    // Returns subtraction of the two numbers
     fn subtract(&self) -> u32 {
-        if self.first_number > self.second_number{
+        if self.first_number > self.second_number {
             self.first_number - self.second_number
-        }
-        else {
+        } else {
             self.second_number - self.first_number
         }
     }
+
+    // This method multiplies numbers using structures to function
+    //
+    // #Arguments
+    //
+    // Instance of the Structure Number
+    //
+    // #Return
+    //
+    // Returns product of the two numbers
     fn multiply(&self) -> u32 {
         self.first_number * self.second_number
     }
 }
 
+// Main function
 fn main() {
     let numbers = Numbers {
         first_number: 30,
@@ -28,5 +59,8 @@ fn main() {
 
     println!("The Addition of the numbers are: {}", numbers.add());
     println!("The Subtraction of the numbers are: {}", numbers.subtract());
-    println!("The Multiplication of the numbers are: {}", numbers.multiply());
+    println!(
+        "The Multiplication of the numbers are: {}",
+        numbers.multiply()
+    );
 }
